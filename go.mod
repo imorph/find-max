cf85eef978c7835a563e451ed657c39126fa209d
Ivan Goncharov <igoncharov@openwaygroup.com>
2019-06-18 09:14:36 +0300

    CHANGE: panic

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/imorph/find-max
 
 go 1.12
 
-require github.com/imorph/find-max/lib/fmax v0.0.0-20190618060819-8964b42fbbb6 // indirect
+require github.com/imorph/find-max/lib/fmax v0.0.0-20190618061042-a83fe21b3818 // indirect
--- lib/fmax/fmax.go
+++ lib/fmax/fmax.go
@@ -7,8 +7,9 @@ import (
 // Max returns max value of provided slice. Panics if provided interface is not slice.
 func Max(slice interface{}) interface{} {
 	v := reflect.ValueOf(slice)
-	if v.Kind() != Slice {
-		panic(&ValueError{Method: "Swapper", Kind: v.Kind()})
+	if v.Kind() != reflect.Slice {
+		//panic(&ValueError{Method: "Swapper", Kind: v.Kind()})
+		panic("AAAA!")
 	}
 	return
 }
