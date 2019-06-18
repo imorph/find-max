a83fe21b3818c0fa028ea4f491193c3bac76f204
Ivan Goncharov <igoncharov@openwaygroup.com>
2019-06-18 09:10:42 +0300

    CHANGE: fix declarations

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/imorph/find-max
 
 go 1.12
 
-require github.com/imorph/find-max/lib/fmax v0.0.0-20190618060628-c59def8476e1 // indirect
+require github.com/imorph/find-max/lib/fmax v0.0.0-20190618060819-8964b42fbbb6 // indirect
--- lib/fmax/fmax.go
+++ lib/fmax/fmax.go
@@ -6,8 +6,9 @@ import (
 
 // Max returns max value of provided slice. Panics if provided interface is not slice.
 func Max(slice interface{}) interface{} {
-	v := ValueOf(slice)
+	v := reflect.ValueOf(slice)
 	if v.Kind() != Slice {
 		panic(&ValueError{Method: "Swapper", Kind: v.Kind()})
 	}
+	return
 }
