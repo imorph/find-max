68681b8c12fde99e048f1c48c06665257b1aaf47
Ivan Goncharov <igoncharov@openwaygroup.com>
2019-06-19 08:31:20 +0300

    CHANGE: fmt

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/imorph/find-max
 
 go 1.12
 
-require github.com/imorph/find-max/lib/fmax v0.0.0-20190619050945-256dc6241f67
+require github.com/imorph/find-max/lib/fmax v0.0.0-20190619052953-2dc7c270b5bf
--- lib/fmax/fmax_test.go
+++ lib/fmax/fmax_test.go
@@ -1,12 +1,12 @@
 package fmax
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestMax001(t *testing.T) {
-	inSlice := []string{"1","2","3","42"}
+	inSlice := []string{"1", "2", "3", "42"}
 	want := "42"
 	got := Max(inSlice, func(i, j int) bool { return inSlice[i] < inSlice[j] })
 	if !reflect.DeepEqual(got, want) {
