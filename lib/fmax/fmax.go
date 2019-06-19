910abd8f8ce6adbb30c0878a5ab46eca0c145cea
Ivan Goncharov <igoncharov@openwaygroup.com>
2019-06-19 07:33:07 +0300

    CHANGE: test

--- lib/fmax/fmax.go
+++ lib/fmax/fmax.go
@@ -2,14 +2,19 @@ package fmax
 
 import (
 	"reflect"
+	"log"
+	"fmt"
 )
 
 // Max returns max value of provided slice. Panics if provided interface is not slice.
 func Max(slice interface{}, comparator func(i, j int) bool) interface{} {
 	v := reflect.ValueOf(slice)
 	if v.Kind() != reflect.Slice {
-		//panic(&ValueError{Method: "Swapper", Kind: v.Kind()})
-		panic("AAAAA!")
+		log.Println("object is not a slice, it is: ", v.Kind())
+		panic("AAAAA! I am in panic!")
 	}
+
+    fmt.Println("incominng slice is: ", v)
+
 	return "mock"
 }
--- main.go
+++ main.go
@@ -12,15 +12,17 @@ func main() {
 		Age  int
 	}
 
-    people := []man{
+	people := []man{
 		{"Gopher", 7},
 		{"Alice", 55},
 		{"Vera", 24},
 		{"Bob", 75},
 	}
-	bigMan := f.Max(people, func(i, j int) bool { return people[i].Name < people[j].Name })
-	fmt.Println("Man with longest name:", bigMan)
+	//bigMan := f.Max(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	//fmt.Println("Man with longest name:", bigMan)
 
-	bigMan = f.Max(people, func(i, j int) bool { return people[i].Age < people[j].Age })
-	fmt.Println("Oldest man:", bigMan)
+	//bigMan = f.Max(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	//fmt.Println("Oldest man:", bigMan)
+
+	bigMan := f.Max(people, func(i, j int) bool { return people[i].Age < people[j].Age })
 }
